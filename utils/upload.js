90e11fc26f89cbc4bf60f19f14435d6202fc674c
Raiyan Nawaz <raiyannawaz@outlook.com>
2025-09-30 13:36:53 +0530

    Update upload.js

--- utils/upload.js
+++ utils/upload.js
@@ -2,20 +2,18 @@ const { extname } = require('path')
 const { uploader } = require('./cloudinary')
 const multer = require('multer')
 
-// const storage = multer.diskStorage({
-//   destination: function(req, file, cb){
-//     cb(null, 'uploads/')
-//   },
-//   filename: function(req, file, cb){
-//     let fileId = new Date().getTime()
-//     let fileFormat = extname(file.originalname.toLowerCase())
-//     let fileName = file.originalname.replace(`${fileFormat}`, '').slice(0, 12)
+const storage = multer.diskStorage({
+  destination: function(req, file, cb){
+    cb(null, 'uploads/')
+  },
+  filename: function(req, file, cb){
+    let fileId = new Date().getTime()
+    let fileFormat = extname(file.originalname.toLowerCase())
+    let fileName = file.originalname.replace(`${fileFormat}`, '').slice(0, 12)
     
-//     cb(null, `${fileName}-${fileId}${fileFormat}`)
-//   }
-// })
-
-const storage = multer.memoryStorage();
+    cb(null, `${fileName}-${fileId}${fileFormat}`)
+  }
+})
 
 const fileFilter = (req, file, cb) =>{
   let allowedFiles = /jpeg|jpg|png|webp/
@@ -36,16 +34,8 @@ const limits = { fileSize: 5 * 1024 * 1024 }
 const upload = multer({storage, fileFilter, limits})
 
 const uploadImage = async (file, folder) =>{
-  // let result = await uploader.upload(file, {folder})
-  // return result
-
-  return await new Promise((resolve, reject)=>{
-    uploader.upload_stream({folder}, 
-      (error, result)=>{
-        if(error) reject(error);
-        else resolve(result)
-      }).end(file)
-  })
+  let result = await uploader.upload(file, {folder})
+  return result
 }
 
 module.exports = { upload, uploadImage }
